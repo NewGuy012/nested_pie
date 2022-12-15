1d0d67d766b673312ae1f0747d4e9b5e95c31bf2
Moses Yoo <myoo@rivian.com>
2022-12-15 11:31:34 -0800

    Add in basic syntax documentation and comments.

--- pie_nested.m
+++ pie_nested.m
@@ -1,23 +1,35 @@
-function p = pie_nested
-%pie_nested
+function varargout = pie_nested(C, varargin)
+%pie_nested Create a nested pie chart.
+%
+% Syntax:
+%   pie_nested(C)
+%   pie_nested(C, Name, Value, ...)
+%   h = pie_nested(_);
+%
+% Documentation:
+%   Please refer to the MathWorks File Exchange or GitHub page for the
+%   detailed documentation and examples.
 
-clc;
-close all;
-
+% Figure properties
 fig = figure;
 background_color = 'w';
-
 fig.Color = background_color;
+
+% Check for output arguments
+if nargout > 1
+    error('Error: Too many output arguments assigned.');
+end
+varargout{1} = fig;
+
+% Axes properties
 ax = gca;
 ax.Color = background_color;
 
-% Axis limits
+% Axis properties
 hold(ax, 'on');
 axis(ax, 'square');
 scaling_factor = 1.2;
 axis(ax, [-1, 1, -1, 1] * scaling_factor);
-
-pie_labels = {'RDU', 'FDU', 'Aero'};
 
 % Axis properties
 ax.XTickLabel = [];
@@ -25,106 +37,195 @@ ax.YTickLabel = [];
 ax.XColor = 'none';
 ax.YColor = 'none';
 
-X = {{0.2, 0.05, 0.25, 0.1, 0.4};...
-    {0.25, 0.25, 0.5}};
+% Pie properties
+num_pie = length(C);
+num_outmost = length(C(end));
+pie_labels = cell(num_outmost, 1);
 
-num_pie = length(X);
+% Number of wedges
+num_wedges = cellfun(@(C) length(C), C, 'UniformOutput', false);
+max_wedges = max(num_wedges);
 
+% Default labels
+for ii = 1:num_outmost
+    pie_labels{ii} = sprintf('Label %i', ii);
+end
+
+% Default arguments
 rho_lower = 0.2;
+edge_color = 'k';
+line_width = 2;
+line_style = '-';
+wedge_colors = lines(max_wedges);
+percent_status = 'on';
+percent_precision = 2;
+label_fontsize = 10;
+fill_transparency = 1;
+label_interpreter = 'none';
+interval_res = 0.01;
+
+% Check if optional arguments were specified
+if numvarargs > 1
+    % Initialze name-value arguments
+    name_arguments = varargin(1:2:end);
+    value_arguments = varargin(2:2:end);
+
+    % Iterate through name-value arguments
+    for ii = 1:length(name_arguments)
+        % Set value arguments depending on name
+        switch lower(name_arguments{ii})
+            case 'rholower'
+                rho_lower = value_arguments{ii};
+            case 'edgecolor'
+                edge_color = value_arguments{ii};
+            case 'linewidth'
+                line_width = value_arguments{ii};
+            case 'linestyle'
+                line_style = value_arguments{ii};
+            case 'wedgecolors'
+                wedge_colors = value_arguments{ii};
+            case 'percentstatus'
+                percent_status = value_arguments{ii};
+            case 'percent_precision'
+                percent_precision = value_arguments{ii};
+            case 'labelfontsize'
+                label_fontsize = value_arguments{ii};
+            case 'filltransparency'
+                fill_transparency = value_arguments{ii};
+            case 'labelinterpreter'
+                label_interpreter = value_arguments{ii};
+            otherwise
+                error('Error: Please enter in a valid name-value pair.');
+        end
+    end
+
+end
+
+% Initialize rho
 rho_upper = 1;
 rho_range = rho_upper - rho_lower;
-
 rho_interval = rho_range/num_pie;
 rho = rho_lower:rho_interval:rho_upper;
 
-c = turbo(8);
-
-iCounter = 0;
-
+% Iterate through number of nested pies
 for ii = 1:num_pie
-    sub_pie = cell2mat(X{ii});
+    % Initialize
+    sub_pie = cell2mat(C{ii}); % Convert from cell to numerical array
+    num_wedge = num_wedges(ii);
+    wedge_color = wedge_colors{ii};
 
+    % Check if data does not sum to one
     if sum(sub_pie) ~= 1
+        % Display warning
         warning('Data does not sum to 1. Attempting to normalize data...');
-        % Normalize data
+        
+        % Normalize data to add up to one
         sub_pie = sub_pie / sum(sub_pie);
     end
 
-    num_wedges = length(sub_pie);
-
+    % Initialize theta
     theta = 2 * pi * sub_pie;
     theta = cumsum(theta);
-    theta = [0, theta];
+    theta = [0, theta]; %#ok<*AGROW> 
 
-    for jj = 1:num_wedges
-        iCounter = iCounter + 1;
-
+    % Iterate through number of wedges
+    for jj = 1:num_wedge
+        % Initialize
         theta_start = theta(jj);
         theta_end = theta(jj+1);
 
         rho_start = rho(ii);
         rho_end = rho(ii+1);
 
+        % Make cyclic
         theta_sides = [theta_start, theta_end, theta_end, theta_start, theta_start];
         rho_sides = [rho_start, rho_start, rho_end, rho_end, rho_start];
 
-        x = [];
-        y = [];
-        interval_res = 0.01;
-
+        % Initialize
+        x_patch = [];
+        y_patch = [];
+        
+        % Iterate through a four-sided polygon
         for kk = 1:4
+            % Initialize
             interval = interval_res;
+
+            % Check if interval needs to be flipped
             if theta_sides(kk) > theta_sides(kk+1)
                 interval = -interval_res;
             end
 
+            % Polar coordinates of polygon
             theta_side = theta_sides(kk):interval:theta_sides(kk+1);
             rho_side = rho_sides(kk) * ones(1, length(theta_side));
 
+            % Convert from polar to cartesian coordinates
             [x_cart, y_cart] = pol2cart(theta_side, rho_side);
 
-            x = [x, x_cart];
-            y = [y, y_cart];
+            % Append to array
+            x_patch = [x_patch, x_cart];
+            y_patch = [y_patch, y_cart];
         end
 
-        patch(x, y, c(iCounter, :),...
-            'EdgeColor', 'k',...
-            'LineWidth', 1);
+        % Create patch object
+        patch(x_patch, y_patch, wedge_color(jj, :),...
+            'LineStyle', line_style,...
+            'EdgeColor', edge_color,...
+            'LineWidth', line_width,...
+            'FaceAlpha', fill_transparency);
     end
 
+    % For all wedges not in the innermost layer
     if ii ~= 1
+        % Find midpoint of theta and rho
         theta_diff = diff(theta)/2;
         theta_diff = theta(1:end-1) + theta_diff;
 
         rho_delta = diff(rho)/2;
         rho_diff = rho(1:end-1) + rho_delta;
-
         rho_diff = rho_diff(2:end);
-
         rho_labels = rho(end) + rho_delta;
 
+        % Iterate through rho midpoints
         for jj = 1:length(rho_diff)
-
-
+            % Initialize
             rho_txt = rho_diff(jj);
             rho_label = rho_labels(jj);
 
+            % Iterate through theta midpoints
             for kk = 1:length(theta_diff)
-                theta_txt = theta_diff(kk);
-                percent_txt = sprintf('%.1f%%', sub_pie(kk)*100);
+                % Check if display percent status
+                if strcmp(percent_status, 'on')
+                    % Initialize
+                    theta_txt = theta_diff(kk);
 
+                    % Format percentage text
+                    precision_txt = sprintf('%i', percent_precision);
+                    percent_txt = sprintf(['%.', precision_txt, 'f%%'], sub_pie(kk)*100);
 
-                [x_txt, y_txt] = pol2cart(theta_txt, rho_txt);
-                text(x_txt, y_txt, percent_txt,...
-                    'Color', 'w',...
-                    'FontWeight', 'bold',...
-                    'HorizontalAlignment', 'center');
+                    % Convert from polar to cartesian
+                    [x_txt, y_txt] = pol2cart(theta_txt, rho_txt);
 
-                [x_label, y_label] = pol2cart(theta_txt, rho_label);
-                text(x_label, y_label, pie_labels{kk},...
-                    'Color', 'k',...
-                    'FontWeight', 'bold',...
-                    'HorizontalAlignment', 'center');
+                    % Display percentage text
+                    text(x_txt, y_txt, percent_txt,...
+                        'Color', 'w',...
+                        'FontWeight', 'bold',...
+                        'HorizontalAlignment', 'center');
+                end
+
+                % Only for outermost
+                if ii == num_pie
+                    % Convert from polar to cartesian
+                    [x_label, y_label] = pol2cart(theta_txt, rho_label);
+
+                    % Display pie labels
+                    text(x_label, y_label, pie_labels{kk},...
+                        'Color', 'k',...
+                        'FontWeight', 'bold',...
+                        'FontSize', label_fontsize,...
+                        'HorizontalAlignment', 'center',...
+                        'Interpreter', label_interpreter);
+                end
 
             end
         end
@@ -133,17 +234,4 @@ for ii = 1:num_pie
     end
 end
 
-title('Nested Pie Chart');
-
-inner_pie = cell2mat(X{1});
-legend_str = cell(length(inner_pie), 1);
-
-for ii = 1:length(inner_pie)
-    legend_str{ii} = sprintf('Wedge #%i: %.2f%%', ii, inner_pie(ii)*100);
-end
-
-lg = legend(legend_str,...
-    'Location', 'southoutside',...
-    'NumColumns', 1);
-lg.Title.String = 'Inner Pie';
 end
